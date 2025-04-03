9d8746e9443389262401452f103c71a6f40f7d6e
dnyaneshvarsultane <107747732+dnyaneshvarsultane@users.noreply.github.com>
2025-04-03 02:17:58 +0000

    backend.tf update

--- .github/workflows/backend.tf
+++ .github/workflows/backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket         = "terraform-state-maintainer"
     key            = "/dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
-    encrypt        = true
   }
 }
