e10aaba2980d57c82d215cda1577363bddd51558
dnyaneshvarsultane <107747732+dnyaneshvarsultane@users.noreply.github.com>
2025-04-03 02:28:28 +0000

    backend.tf update

--- .github/workflows/backend.tf
+++ .github/workflows/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-maintainer"
-    key            = "/dev/terraform.tfstate"
+    key            = "dev/terraform.tfstate"
     region         = "us-east-1"
   }
 }
